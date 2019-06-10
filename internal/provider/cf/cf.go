29c06c5b46ab9b4d689e55620fe5e02d1fe80689
DingGGu <ggu@dunamu.com>
2019-06-10 15:48:07 +0900

    Added test

--- internal/provider/cf/cf.go
+++ internal/provider/cf/cf.go
@@ -8,8 +8,20 @@ import (
 	"strings"
 )
 
+type CloudflareInterface interface {
+	ZoneIDByName(zoneName string) (string, error)
+	AccessApplications(zoneID string, pageOpts cloudflare.PaginationOptions) ([]cloudflare.AccessApplication, cloudflare.ResultInfo, error)
+	CreateAccessApplication(zoneID string, accessApplication cloudflare.AccessApplication) (cloudflare.AccessApplication, error)
+	UpdateAccessApplication(zoneID string, accessApplication cloudflare.AccessApplication) (cloudflare.AccessApplication, error)
+	DeleteAccessApplication(zoneID, applicationID string) error
+	AccessPolicies(zoneID, applicationID string, pageOpts cloudflare.PaginationOptions) ([]cloudflare.AccessPolicy, cloudflare.ResultInfo, error)
+	CreateAccessPolicy(zoneID, applicationID string, accessPolicy cloudflare.AccessPolicy) (cloudflare.AccessPolicy, error)
+	UpdateAccessPolicy(zoneID, applicationID string, accessPolicy cloudflare.AccessPolicy) (cloudflare.AccessPolicy, error)
+	DeleteAccessPolicy(zoneID, applicationID, accessPolicyID string) error
+}
+
 type CloudFlareProvider struct {
-	api             *cloudflare.API
+	Api             CloudflareInterface // *cloudflare.API
 	cachedZoneIdMap map[string]string
 }
 
@@ -20,7 +32,7 @@ func GetCloudFlareClient(cfApiKey string, cfApiEmail string) *CloudFlareProvider
 		os.Exit(1)
 	}
 	return &CloudFlareProvider{
-		api: api,
+		Api: api,
 	}
 }
 
@@ -34,7 +46,7 @@ func (cf CloudFlareProvider) getZoneIDByName(zoneName string) string {
 		return val
 	}
 
-	zoneId, err := cf.api.ZoneIDByName(zoneName) // Assuming example.com exists in your Cloudflare account already
+	zoneId, err := cf.Api.ZoneIDByName(zoneName) // Assuming example.com exists in your Cloudflare account already
 	if err != nil {
 		logrus.Error(err)
 		os.Exit(1)
@@ -47,7 +59,7 @@ func (cf CloudFlareProvider) getZoneIDByName(zoneName string) string {
 
 func (cf CloudFlareProvider) GetAccessApplications(zoneName string) map[string]option.AccessApp {
 	zoneId := cf.getZoneIDByName(zoneName)
-	applications, _, err := cf.api.AccessApplications(zoneId, cloudflare.PaginationOptions{})
+	applications, _, err := cf.Api.AccessApplications(zoneId, cloudflare.PaginationOptions{})
 
 	if err != nil {
 		logrus.Error(err)
@@ -58,6 +70,7 @@ func (cf CloudFlareProvider) GetAccessApplications(zoneName string) map[string]o
 
 	for _, app := range applications {
 		split := strings.Split(app.Name, "-")
+		// Get Managed Cloudflare Accesses
 		if len(split) == 3 {
 			clusterUID, namespace, ingressName := split[0], split[1], split[2]
 			cfOpt := option.AccessAppOption{
@@ -80,22 +93,22 @@ func (cf CloudFlareProvider) GetAccessApplications(zoneName string) map[string]o
 
 func (cf CloudFlareProvider) UpdateAccessApplication(zoneName string, accessApp cloudflare.AccessApplication) (cloudflare.AccessApplication, error) {
 	zoneId := cf.getZoneIDByName(zoneName)
-	return cf.api.UpdateAccessApplication(zoneId, accessApp)
+	return cf.Api.UpdateAccessApplication(zoneId, accessApp)
 }
 
 func (cf CloudFlareProvider) DeleteAccessApplication(zoneName string, appId string) error {
 	zoneId := cf.getZoneIDByName(zoneName)
-	return cf.api.DeleteAccessApplication(zoneId, appId)
+	return cf.Api.DeleteAccessApplication(zoneId, appId)
 }
 
 func (cf CloudFlareProvider) CreateAccessApplication(zoneName string, accessApp cloudflare.AccessApplication) (cloudflare.AccessApplication, error) {
 	zoneId := cf.getZoneIDByName(zoneName)
-	return cf.api.CreateAccessApplication(zoneId, accessApp)
+	return cf.Api.CreateAccessApplication(zoneId, accessApp)
 }
 
 func (cf CloudFlareProvider) GetAccessPolicies(zoneName string, accessAppId string) []cloudflare.AccessPolicy {
 	zoneId := cf.getZoneIDByName(zoneName)
-	policies, _, err := cf.api.AccessPolicies(zoneId, accessAppId, cloudflare.PaginationOptions{})
+	policies, _, err := cf.Api.AccessPolicies(zoneId, accessAppId, cloudflare.PaginationOptions{})
 
 	if err != nil {
 		logrus.Error(err)
@@ -107,15 +120,15 @@ func (cf CloudFlareProvider) GetAccessPolicies(zoneName string, accessAppId stri
 
 func (cf CloudFlareProvider) CreateAccessPolicy(zoneName string, accessAppId string, accessPolicy cloudflare.AccessPolicy) (cloudflare.AccessPolicy, error) {
 	zoneId := cf.getZoneIDByName(zoneName)
-	return cf.api.CreateAccessPolicy(zoneId, accessAppId, accessPolicy)
+	return cf.Api.CreateAccessPolicy(zoneId, accessAppId, accessPolicy)
 }
 
 func (cf CloudFlareProvider) UpdateAccessPolicy(zoneName string, accessAppId string, accessPolicy cloudflare.AccessPolicy) (cloudflare.AccessPolicy, error) {
 	zoneId := cf.getZoneIDByName(zoneName)
-	return cf.api.UpdateAccessPolicy(zoneId, accessAppId, accessPolicy)
+	return cf.Api.UpdateAccessPolicy(zoneId, accessAppId, accessPolicy)
 }
 
 func (cf CloudFlareProvider) DeleteAccessPolicy(zoneName string, accessAppId string, accessPolicyId string) error {
 	zoneId := cf.getZoneIDByName(zoneName)
-	return cf.api.DeleteAccessPolicy(zoneId, accessAppId, accessPolicyId)
+	return cf.Api.DeleteAccessPolicy(zoneId, accessAppId, accessPolicyId)
 }
