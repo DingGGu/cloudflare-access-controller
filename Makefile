338208d28e4f0c7eb0088a7dd3c2ecd44c35e0c2
DingGGu <ggu@dunamu.com>
2020-09-18 17:25:41 +0900

    Update go runtime to 1.15

--- Makefile
+++ Makefile
@@ -9,4 +9,4 @@ build:
 	CGO_ENABLED=0 GOOS=$(OS) GOARCH=$(ARCH) go build -a -ldflags "-s -X $(PKG)/version.COMMIT=${GIT_COMMIT} -X $(PKG)/version.RELEASE=${GIT_TAG} -X $(PKG)/version.REPO=${GIT_REPO}" -o cloudflare-access-controller ./cmd
 
 docker:
-	docker build -t dingggu/cloudflare-access-controller .
+	docker build -t ghcr.io/dingggu/cloudflare-access-controller .
--- cmd/main.go
+++ cmd/main.go
@@ -7,18 +7,16 @@ import (
 	"os"
 	runtime "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
-	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 )
 
-var log = logf.Log.WithName("cloudflare-access-controller")
-
 func main() {
 	options := getOptions()
-	logf.SetLogger(zap.New(zap.UseDevMode(options.Debug)))
+	log.SetLogger(zap.New(zap.UseDevMode(options.Debug)))
 
-	logger := log.WithName("main")
+	logger := log.Log.WithName("main")
 
 	mgr, err := manager.New(config.GetConfigOrDie(), manager.Options{
 		Namespace:              options.WatchNamespace,
@@ -33,14 +31,14 @@ func main() {
 
 	provider := providers.NewCloudflare(
 		options.CloudflareApiToken,
-		logf.Log.WithName("providers").WithName("cloudflare"),
+		log.Log.WithName("providers").WithName("cloudflare"),
 		options.ZoneName,
 		options.ClusterName,
 	)
 
 	if err := (&controllers.Controller{
 		Client:   mgr.GetClient(),
-		Log:      logf.Log.WithName("controllers").WithName("ingress"),
+		Log:      log.Log.WithName("controllers").WithName("ingress"),
 		Recorder: mgr.GetEventRecorderFor("cloudflare-access-controller"),
 		Provider: provider,
 	}).New(mgr); err != nil {
@@ -52,7 +50,7 @@ func main() {
 	_ = mgr.AddReadyzCheck("check", func(_ *http.Request) error { return nil })
 
 	if err := mgr.Start(runtime.SetupSignalHandler()); err != nil {
-		log.Error(err, "could not start manager")
+		log.Log.Error(err, "could not start manager")
 		os.Exit(1)
 	}
 }
